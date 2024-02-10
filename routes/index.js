e76c9511d913cadc57d2804f49b56cd079ee9089
Billy P <itisbilly@icloud.com>
2024-02-09 23:52:37 -0800

    Added a place for the messages to be saved in besides memory

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,34 @@
 var express = require('express');
 var router = express.Router();
+const fs = require('fs');
 
-const messages = [
-  {
-    text: "Hi there!",
-    user: "Amando",
-    date: new Date(),
-    id: 0
-  },
-  {
-    text: "Hello World!",
-    user: "Charles",
-    date: new Date(),
-    id: 1
+// const messages = [
+//   {
+//     text: "Hi there!",
+//     user: "Amando",
+//     date: new Date(),
+//     id: 0
+//   },
+//   {
+//     text: "Hello World!",
+//     user: "Charles",
+//     date: new Date(),
+//     id: 1
+//   }
+// ];
+
+const filePath = "messages.json";
+const messages = [];
+
+fs.readFile(filePath, 'utf8', (err, data) => {
+  console.log("Messages are empty");
+  if (err) {
+    console.error('Error reading file:', err);
+    return;
   }
-];
+  messages.push(...JSON.parse(data));
+  console.log("Messages have been loaded");
+})
 
 const siteTitle = 'Mini Message Board';
 const arrayLength = messages.length;
@@ -35,6 +49,14 @@ router.post('/new', function(req, res, next){
     date: new Date(),
     id: messages.length
   });
+  fs.writeFile('messages.json', JSON.stringify(messages), (err) => {
+    if (err) {
+      console.error("Error writing JSON file", err);
+      return;
+    }
+    console.log("Messages saved successfully");
+  })
+
   res.redirect('/')
 });
 
